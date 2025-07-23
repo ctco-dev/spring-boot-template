8f0d2e38ccf99e7e9f8fb3e9348a6b9562a3866e
victor <viktor@nmmlm.com>
2025-07-23 20:44:43 +0300

    fix: Support class was breaking the build by relying on JUnit containers

--- src/test/java/lv/ctco/springboottemplate/common/MongoDbContainerTestSupport.java
+++ src/test/java/lv/ctco/springboottemplate/common/MongoDbContainerTestSupport.java
@@ -1,19 +1,20 @@
 package lv.ctco.springboottemplate.common;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestConstructor;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
 @Testcontainers
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-public class MongoDbContainerTestSupport {
+public abstract class MongoDbContainerTestSupport {
 
-  @Container static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.8");
+  protected static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.8");
+
+  static {
+    mongoDBContainer.start();
+  }
 
   @DynamicPropertySource
   static void setProperties(DynamicPropertyRegistry registry) {
