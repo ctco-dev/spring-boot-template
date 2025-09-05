5a9107a38bede2e166169030a413dffc4db0d458
victor <viktor@nmmlm.com>
2025-09-05 18:44:47 +0300

    Revert "chore: WIP, working stats, without details"
    This reverts commit 95f70aee9ee48362dcf971637157f84a0a3614dc.

--- src/main/java/lv/ctco/springboottemplate/features/statistics/dto/StatsFormatEnum.java
+++
@@ -1,6 +0,0 @@
-package lv.ctco.springboottemplate.features.statistics.dto;
-
-public enum StatsFormatEnum {
-  SUMMARY,
-  DETAILED
-}
--- src/main/java/lv/ctco/springboottemplate/features/statistics/dto/TodoBreakdownDTO.java
+++
@@ -1,5 +0,0 @@
-package lv.ctco.springboottemplate.features.statistics.dto;
-
-import java.util.List;
-
-public record TodoBreakdownDTO(List<TodoItemDTO> completed, List<TodoItemDTO> pending) {}
