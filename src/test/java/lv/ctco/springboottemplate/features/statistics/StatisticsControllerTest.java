831c496cd9a9eca9f466e154f5d472e6059bd8fc
Aleksandrs Semjonovs <warlockxins@gmail.com>
2025-10-28 18:48:12 +0200

    try fixing test - mongo container added

--- src/test/java/lv/ctco/springboottemplate/features/statistics/StatisticsControllerTest.java
+++ src/test/java/lv/ctco/springboottemplate/features/statistics/StatisticsControllerTest.java
@@ -18,17 +18,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-// import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-// @Testcontainers
+@Testcontainers
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class StatisticsControllerTest {
+  @Container static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.8");
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+  }
+
   private final MockMvc mockMvc;
 
   private final ObjectMapper objectMapper;
