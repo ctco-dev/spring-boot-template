62f9ec71f7a65a56876dd55b5398e2a0b61beca6
ViktorDolzenko <vitja1234@gmail.com>
2026-02-17 14:15:36 +0200

    Viktors Dolzenko task/1-2 global exception

---
+++ src/main/java/lv/ctco/springboottemplate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package lv.ctco.springboottemplate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Oops, something went wrong, try later");
+  }
+}
